e1bc6df614537fee3f9be00f58473d917353cf3f
Alan Andrade <alan.andradec@gmail.com>
2015-12-14 00:13:50 -0800

    WIPpu

--- lib/hanoi.ex
+++ lib/hanoi.ex
@@ -1,21 +1,59 @@
 defmodule Hanoi do
   def solve n do
-    IO.puts "Hanoi of : #{n}"
     moveTower(n, [:A, :B, :C])
   end
 
-  def moveTower(0, _), do: []
+  def steps n do
+    [ size: n, moves: solve(n) ]
+  end
 
-  def moveTower(n, [source, dest, via]) when n > 0 do
+  defp moveTower(0, _), do: []
+  defp moveTower(n, [source, dest, via]) when n > 0 do
     moveTower(n - 1, [source, via, dest]) ++
     moveDisk([source, dest]) ++
     moveTower(n - 1, [via, dest, source])
   end
 
-  def moveDisk [a, b] do
-    IO.puts "#{a} -> #{b}"
-    [[a, b]]
+  defp moveDisk([a, b]), do: [[a, b]]
+
+  def inject_data size: n, moves: moves do
+    # [
+    # [:A, :b]  [{:A, a}, {:B, b}, {:C, b}]
+    moves |>
+    inject_data( A: Enum.to_list(1..n),
+                 B: [],
+                 C: [])
   end
 
+  def inject_data moves, pegs do
+    moves |>
+    Enum.reduce(pegs, fn (move, acc) ->
+      [source | dest] = move
+      via = ([:A, :B, :C] -- [source, dest])
 
+      IO.inspect via
+      IO.inspect [source, dest, via]
+
+      [ { ^source, source_tower },
+        { ^dest, dest_tower },
+        { ^via, via_tower }
+      ] = pegs
+
+      if length(pegs[source]) > 0 do
+        [
+          { source, tl(source_tower) },
+          { dest, [ hd(source_tower) | dest_tower ] },
+          { via, via_tower }
+        ]
+      else
+        [
+          { source, source_tower },
+          { dest, dest_tower },
+          { via, via_tower }
+        ]
+      end
+    end) |> Enum.map(fn (move, [A: a, B: b, C: c ]) ->
+      { move, a, b, c }
+    end)
+  end
 end
--- test/hanoi_test.exs
+++ test/hanoi_test.exs
@@ -1,10 +1,17 @@
 defmodule HanoIexTests do
   use ExUnit.Case
 
-  test "alorithm count" do
+  test "alorithm tests" do
     assert Hanoi.solve(1) |> Enum.count == :math.pow(2 , 1) - 1
     assert Hanoi.solve(2) |> Enum.count == :math.pow(2 , 2) - 1
     assert Hanoi.solve(3) |> Enum.count == :math.pow(2 , 3) - 1
     assert Hanoi.solve(4) |> Enum.count == :math.pow(2 , 4) - 1
+  end
+
+  test "moves" do
+    assert Hanoi.steps(2) |>
+           Hanoi.inject_data == [
+             { [:start], [1], [ ], [ ] },
+             { [:A, :C], [ ], [ ], [1] }]
   end
 end
