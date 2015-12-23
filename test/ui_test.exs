fe5c23d71b9c22e9c6781b02ce2a158f76f2d1ca
Alan Andrade <alan.andradec@gmail.com>
2015-12-22 18:00:35 -0800

    WIP on drawing

--- test/ui_test.exs
+++ test/ui_test.exs
@@ -2,17 +2,27 @@ defmodule UITest do
   use ExUnit.Case
 
   defmodule UI do
-    def draw :peg, data do
-      # [1]
-      # width = magn * 
+    def draw [], n do
+      ' | '
+    end
+
+    def draw disk, n do
+      spaces = if disk > n, do: 1..n - disk, else: []
+      fill   = 1..(disk * 2 + 1)
+
+      "" |>
+      Enum.map(fn (x) -> for _c <- spaces, do: x <> " " end) |>
+      Enum.map(fn (x) -> for _c <- fill, do: x <> "#" end)
+      #for _c <- spaces, into: collector, do: " "
     end
   end
 
-  test "draw tower" do
-    result = UI.draw(:peg, [1, 2])
-
-    IO.puts result
-    assert result == '  x  \n' ++
-                     ' xxx \n'
+  test "render frame" do
+    assert UI.draw([], 1 ) == ' | '
+    assert UI.draw(1, 1) == "###"
+    assert UI.draw(1, 2) == " ### "
+    assert UI.draw(2, 2) == "#####"
+    assert UI.draw(1, 3) == "  ###  "
+    assert UI.draw(2, 3) == " ##### "
   end
 end
