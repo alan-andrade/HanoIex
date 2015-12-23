867362dbb6793e034cd79e40c2bc1e8f2f2e5022
Alan Andrade <alan.andradec@gmail.com>
2015-12-22 22:22:14 -0800

    UI.draw_peg with awful impl.

--- test/ui_test.exs
+++ test/ui_test.exs
@@ -2,27 +2,63 @@ defmodule UITest do
   use ExUnit.Case
 
   defmodule UI do
-    def draw [], n do
-      ' | '
+    def draw_peg({ [], height }, level: _) do
+      space = for _c <- 1..height, into: "", do: " "
+      space <> "|" <> space
     end
 
-    def draw disk, n do
-      spaces = if disk > n, do: 1..n - disk, else: []
-      fill   = 1..(disk * 2 + 1)
+    def draw_peg peg, level: level do
+      { stack, height } = peg
 
-      "" |>
-      Enum.map(fn (x) -> for _c <- spaces, do: x <> " " end) |>
-      Enum.map(fn (x) -> for _c <- fill, do: x <> "#" end)
-      #for _c <- spaces, into: collector, do: " "
+      val = Enum.at(Enum.reverse(stack), level)
+
+      if is_nil val do
+        draw_peg({[], height}, level: level)
+      else
+        w = height - val
+        space = if w > 0 do
+          for _c <- 1..w, into: "", do: " "
+        else
+          ""
+        end
+
+        side = for _c <- 1..val, into: "", do: "#"
+        disk = "#"
+        space <> side <> disk <> side <> space
+      end
     end
+
   end
 
   test "render frame" do
-    assert UI.draw([], 1 ) == ' | '
-    assert UI.draw(1, 1) == "###"
-    assert UI.draw(1, 2) == " ### "
-    assert UI.draw(2, 2) == "#####"
-    assert UI.draw(1, 3) == "  ###  "
-    assert UI.draw(2, 3) == " ##### "
+    peg = { [], 2 }
+    assert UI.draw_peg(peg, level: 1) == "  |  "
+    assert UI.draw_peg(peg, level: 0) == "  |  "
+
+    peg = { [1], 2 }
+    assert UI.draw_peg(peg, level: 1) == "  |  "
+    assert UI.draw_peg(peg, level: 0) == " ### "
+
+    peg = { [2], 2 }
+    assert UI.draw_peg(peg, level: 1) == "  |  "
+    assert UI.draw_peg(peg, level: 0) == "#####"
+
+    peg = { [1, 2], 2 }
+    assert UI.draw_peg(peg, level: 1) == " ### "
+    assert UI.draw_peg(peg, level: 0) == "#####"
+
+    peg = { [1, 2], 2 }
+    assert UI.draw_peg(peg, level: 1) == " ### "
+    assert UI.draw_peg(peg, level: 0) == "#####"
+
+    peg = { [1, 2, 3], 3 }
+    assert UI.draw_peg(peg, level: 2) == "  ###  "
+    assert UI.draw_peg(peg, level: 1) == " ##### "
+    assert UI.draw_peg(peg, level: 0) == "#######"
+
+    peg = { [2], 3 }
+    assert UI.draw_peg(peg, level: 2) == "   |   "
+    assert UI.draw_peg(peg, level: 1) == "   |   "
+    assert UI.draw_peg(peg, level: 0) == " ##### "
   end
 end
