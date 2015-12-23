2fe8d44a2268868fb1893cb43077c4db42dc5b20
Alan Andrade <alan.andradec@gmail.com>
2015-12-23 01:02:08 -0800

    WIP on frames

--- test/ui_test.exs
+++ test/ui_test.exs
@@ -2,6 +2,22 @@ defmodule UITest do
   use ExUnit.Case
 
   defmodule UI do
+    def frame pegs do
+      { _, size } = hd pegs
+
+      IO.inspect size
+
+      for level <- (size-1)..0 do
+        Enum.reduce(pegs, "", fn(peg, acc) ->
+          draw(peg, level: level) ++ acc
+        end)
+      end
+    end
+
+    def draw pegs, level: level do
+      for peg <- pegs, into: "", do: UI.draw_peg(peg, level: level)
+    end
+
     def draw_peg {stack, height}, level: level do
       size = Enum.reverse(stack) |> Enum.at(level, 0)
 
@@ -15,7 +31,7 @@ defmodule UITest do
     defp repeat(str, n), do: for _ <- 1..n, into: "", do: str
   end
 
-  test "render frame" do
+  test "render peg" do
     peg = { [], 2 }
     assert UI.draw_peg(peg, level: 1) == "  |  "
     assert UI.draw_peg(peg, level: 0) == "  |  "
@@ -45,5 +61,27 @@ defmodule UITest do
     assert UI.draw_peg(peg, level: 2) == "   |   "
     assert UI.draw_peg(peg, level: 1) == "   |   "
     assert UI.draw_peg(peg, level: 0) == " ##### "
+  end
+
+  test "render collection of pegs" do
+    pegs = [ { [1], 2},
+             { [],  2} ]
+
+    assert UI.draw(pegs, level: 1) == "  |    |  "
+    assert UI.draw(pegs, level: 0) == " ###   |  "
+
+    pegs = [ { [1, 2], 2},
+             { [],     2} ]
+
+    assert UI.draw(pegs, level: 1) == " ###   |  "
+    assert UI.draw(pegs, level: 0) == "#####  |  "
+  end
+
+  test "render game" do
+    pegs = [ { [1], 2},
+             { [],  2} ]
+
+    assert UI.frame(pegs) == "  |    |  " <>
+                             " ###   |  "
   end
 end
